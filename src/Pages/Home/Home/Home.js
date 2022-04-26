7a1a36a12afef1f5b4a66bbf5cfc7a59eb6acb96
iinaamasum <iinaamasum@gmail.com>
2022-04-26 13:55:08 +0600

    footer components done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../../Shared/Footer/Footer';
 import Navbar from '../../Shared/Navbar/Navbar';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 };
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,7 +1,102 @@
 import React from 'react';
+import { AiFillGithub } from 'react-icons/ai';
+import { BsFacebook, BsLinkedin } from 'react-icons/bs';
+import { GrTwitter } from 'react-icons/gr';
+import { Link } from 'react-router-dom';
+import logo from '../../../images/logos/Group 1329.png';
 
 const Footer = () => {
-  return <div>footer</div>;
+  const Year = new Date().getFullYear();
+  const links = [
+    { id: 1, path: '/', name: 'Home' },
+    { id: 2, path: '/donation', name: 'Donation' },
+    { id: 3, path: '/events', name: 'Events' },
+    { id: 4, path: '/blogs', name: 'Blog' },
+  ];
+  return (
+    <>
+      <div className="bg-black">
+        <div
+          style={{ maxWidth: '1200px' }}
+          className="container mx-auto flex justify-between py-2"
+        >
+          <div className="text-white">
+            <Link to="/">
+              <img className="h-16" src={logo} alt="" />
+            </Link>
+            <p className="mt-5">&copy; {Year} All rights reserved.</p>
+            <p>
+              Developed by{' '}
+              <a
+                href="https://github.com/iinaamasum"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="text-red-500">iinaamasum</span>
+              </a>
+            </p>
+          </div>
+          <div className="text-white flex justify-center items-center">
+            <a
+              href="https://www.facebook.com/iinaamasum"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsFacebook
+                size={40}
+                className="mr-3 cursor-pointer hover:text-slate-300"
+              />
+            </a>
+            <a
+              href="https://github.com/iinaamasum"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <AiFillGithub
+                size={40}
+                className="mr-3 cursor-pointer hover:text-slate-300"
+              />
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/iinaamasum"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsLinkedin
+                size={40}
+                className="mr-3 cursor-pointer hover:text-slate-300"
+              />
+            </a>
+            <a
+              href="https://twitter.com/iinaamasum"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GrTwitter
+                size={40}
+                className="mr-3 cursor-pointer hover:text-slate-300"
+              />
+            </a>
+          </div>
+          <div className="">
+            <h4 className="text-blue-500 text-xl my-2">Shortcuts links</h4>
+            <div className="flex flex-col">
+              {links.map((link) => (
+                <Link
+                  className="mr-0 md:mr-10 text-md font-semibold text-gray-400 hover:text-red-800 transition-all duration-150 ease-in-out"
+                  to={link.path}
+                  key={link.id}
+                >
+                  {link.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
